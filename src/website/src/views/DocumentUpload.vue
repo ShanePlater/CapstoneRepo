ffb899c7c4895a911778b2edfad91c261cfde433
ShanePlater <shaneplater92@gmail.com>
2019-09-15 16:02:41 +1000

    updating repo with current code

--- src/website/src/views/DocumentUpload.vue
+++ src/website/src/views/DocumentUpload.vue
@@ -33,7 +33,7 @@ what this page needs:
           
           <!-- document information -->
             <el-form-item label="Select File:">              
-             <el-upload action="" :auto-upload="false" :on-change="handleChange" :file-list="files">
+             <el-upload action="" :auto-upload="false" :multiple="false" :on-change="handleChange" :file-list="files"> <!-- change on-change to on-exceed -->
               <el-button slot="trigger" size="small" type="primary">Select file</el-button>
               </el-upload>
             </el-form-item>            
