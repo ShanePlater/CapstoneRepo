0a46c088647c72c5b67ff8daca5e7e2859a9c7c7
okmiddle <angusnobbs@hotmail.com>
2019-09-18 09:42:55 +1000

    Bug testing Site Inspection
    Site Inspection bug testing

--- src/website/src/views/SiteInspection.vue
+++ src/website/src/views/SiteInspection.vue
@@ -2,16 +2,7 @@
   <section>
       <h1>New Site Inspection Record</h1>
       <br>
-      <el-row>
-        <el-col :span="12">
           <el-form ref="form" :model="form" label-width="12.5em" label-position="left">
-
-
-           <!-- CLIENT INFORMATION  --> 
-          <h2 style="font-size:20px"> Enter Site Inspection Record </h2>
-          
-          <!-- client information -->
-
 
             <el-form-item label="Project Number">
               <el-input v-model="form.ProjectNumber"></el-input>
@@ -33,15 +24,7 @@
             <el-form-item label="Details">
               <el-input v-model="form.Details" type="textarea"></el-input>
             </el-form-item>
-            
-
-            <!-- shane fix this -->
-            <el-form-item>
-              <el-button type="primary" @click="redirecting">Add New Site Inspection</el-button>
-            </el-form-item>
           </el-form>
-        </el-col>
-      </el-row>
   </section>
 </template>
 
