6a5c8f6301b1aa692bb259f4d16ca13e640c97dc
Matthew Lord <mattlord09@hotmail.com>
2019-09-18 01:39:25 +1000

    adding back helpdesk

---
+++ src/website/src/views/SiteInspection.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h1>Helpdesk</h1>
+    <br>
+    <el-row>
+    <el-button type="primary" @click="contactHelpdesk">Contact Helpdesk</el-button>
+    </el-row>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'siteinspection',
+  components: {},
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
