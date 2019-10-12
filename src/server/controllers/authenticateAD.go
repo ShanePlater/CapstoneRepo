a21582f6dd90c91bd23177b9d05b5df3cce7d681
Matthew Lord <mattlord09@hotmail.com>
2019-10-12 14:10:13 +1000

    changed the DC that we are connecting to

--- src/server/controllers/authenticateAD.go
+++ src/server/controllers/authenticateAD.go
@@ -22,7 +22,7 @@ func authenticateAD(g *gin.Context, m *models.Context) {
 	}
 
 	config := &auth.Config{
-		Server:   "LR-BNE-DC-01",        /* Box that contains domain controller */
+		Server:   "LR-BNE-ERP-01",       /* Box that contains domain controller */
 		Port:     389,                   /* Default Port for AD connection */
 		BaseDN:   "OU=lar,DC=net,DC=au", /* Domain */
 		Security: auth.SecurityStartTLS,
